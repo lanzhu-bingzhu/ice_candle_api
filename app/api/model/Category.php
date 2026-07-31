92710db0bd91e01d3a6933df54280f3e5bea08b2
dongliaoyu <2320697174@qq.com>
2026-07-31 17:21:19 +0800

    添加对目录页面的支持

--- app/api/model/Category.php
+++ app/api/model/Category.php
@@ -11,12 +11,24 @@ class Category extends BaseModel
         return $this->hasMany('Category', 'parent_id', 'category_id');
     }
 
-    public function getList($parent_id = 0) {
-        $data = $this->where('parent_id', $parent_id)->where('is_show', 1)->select();
+    public function getList($category_id, $parent_id = 0) {
+        $map = $this->getMap($category_id, $parent_id);
+        $data = $this->where($map)->where('is_show', 1)->select();
         foreach ($data as $key => $value) {
             $data[$key]['type'] = CategoryType::where('category_type_id', $value['type_id'])->value('name');
         }
         return $data;
+    }
+
+    public function getMap($category_id, $parent_id = 0) {
+        $map = [];
+        if ($category_id) {
+            $map[] = ['category_id', '=', $category_id];
+        }
+        if ($parent_id) {
+            $map[] = ['parent_id', '=', $parent_id];
+        }
+        return $map;
     }
 
     public function getDetail($id) {
