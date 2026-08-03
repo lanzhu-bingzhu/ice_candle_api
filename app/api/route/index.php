9e444aa477e463840ad2e1de1d1d8e1c63b85ccd
dongliaoyu <2320697174@qq.com>
2026-08-03 17:36:59 +0800

    目录页改造和文章页改造

--- app/api/route/index.php
+++ app/api/route/index.php
@@ -4,11 +4,10 @@ use think\facade\Route;
 Route::rule('v1/task/:name', 'Task/read', 'GET');
 Route::rule('v1/task', 'Task/index', 'GET');
 
-Route::rule('v1/category/:category_id/post', 'Post/index', 'GET');
-
 Route::rule('v1/category/:id', 'Category/read', 'GET');
 Route::rule('v1/category', 'Category/index', 'GET');
 
 Route::rule('v1/floor', 'Floor/index', 'GET');
 
 Route::rule('v1/post/:id', 'Post/read', 'GET');
+Route::rule('v1/post', 'Post/index', 'GET');
